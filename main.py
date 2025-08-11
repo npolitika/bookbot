6b61d93510315978141845feee7788881c083dee
npolitika <github.proving322@passinbox.com>
2025-08-11 09:15:52 -0400

    Added word count

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 def get_book_text(filepath):
     with open(filepath) as booktext:
-        return booktext.read()
+        book_text = booktext.read()
+        return book_text
+
+def wordcount(book_text):
+    words = book_text.split()
+    word_count = 0
+    for i in words:
+        word_count += 1
+    return word_count
 
 def main():
     book_path = "books/frankenstein.txt"
     book_contents = get_book_text(book_path)
-    print(book_contents)
+    word_count = wordcount(book_contents)
+    print(f'{word_count} words found in the document')
 
 main()
